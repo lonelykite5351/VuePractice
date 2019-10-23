47b625f470f2a93a3491f84dc0e3b81967da3945
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2019-10-24 00:50:59 +0800

    vue元件資料傳達練習1

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,82 @@
 #app
   h1 記帳程式
   hr
-  ul.record
-    li
+  RecordList(v-bind:records="records")
+  h2 第二個人
+  RecordList(v-bind:records="records2")
 
 </template>
 
 <script>
+import RecordList from './components/RecordList.vue'
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to lonelykite Vue.js App'
+      msg: 'Welcome to lonelykite Vue.js App',
+      records: [
+        {
+          date: "2019/10/22",
+          price: 100,
+          cata: "Apple"
+        },
+        {
+          date: "2019/10/22",
+          price: 200,
+          cata: "iPhone 8"
+        },
+        {
+          date: "2019/10/22",
+          price: 300,
+          cata: "iPhone 9"
+        },
+        {
+          date: "2019/10/22",
+          price: 400,
+          cata: "iPhone 10"
+        },
+        {
+          date: "2019/10/22",
+          price: 500,
+          cata: "iPhone 11"
+        },
+      ],
+      records2: [
+        {
+          date: "2019/10/23",
+          price: 150,
+          cata: "Apple"
+        },
+        {
+          date: "2019/10/23",
+          price: 20000,
+          cata: "iPhone 8"
+        },
+        {
+          date: "2019/10/23",
+          price: 3000,
+          cata: "iPhone 9"
+        },
+        {
+          date: "2019/10/23",
+          price: 4258570,
+          cata: "iPhone 10"
+        },
+        {
+          date: "2019/10/23",
+          price: 55400,
+          cata: "iPhone 11"
+        },
+      ]
     }
+  },
+  components: {
+    RecordList: RecordList
   }
 }
 </script>
 
 <style lang="sass">
 #app 
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
-
-h1, h2 
-  font-weight: normal
-
-ul 
-  list-style-type: none
-  padding: 0
-
-li 
-  display: inline-block
-  margin: 0 10px
-
-a 
-  color: #42b983
-
+  
 </style>
---
+++ src/components/RecordList.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  ul.record
+    li(v-for="record in records")
+      span {{record.date}}
+      span {{record.price}} 元
+      span {{record.cata}}
+</template>
+
+<script>
+export default {
+    props: ["records"],
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
